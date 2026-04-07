824dfcbed875f287b2f3462fad75761ab74c4a1b
Xiao Hanyu <xiaohanyu1988@gmail.com>
2026-04-07 20:17:02 +0800

    chore: merge the original next.js app with payload frontend
    Use the existing metadata setup for payload created layout.tsx

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,19 +1,79 @@
+import type { Metadata } from 'next'
 import type React from 'react'
+import { siteConfig } from '@/config/site'
 import './styles.css'
 
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+const defaultTitle = `${siteConfig.name} - ${siteConfig.description}`
+
+export const metadata: Metadata = {
+  metadataBase: new URL(siteConfig.url),
+  title: {
+    default: defaultTitle,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  authors: siteConfig.authors,
+  creator: siteConfig.creator.name,
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.description,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: defaultTitle,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: siteConfig.creator.twitter,
+  },
+  icons: {
+    icon: [
+      {
+        url: '/static/favicons/favicon-96x96.png',
+        type: 'image/png',
+        sizes: '96x96',
+      },
+      {
+        url: '/static/favicons/favicon.svg',
+        type: 'image/svg+xml',
+      },
+    ],
+    shortcut: {
+      rel: 'shortcut icon',
+      url: '/static/favicons/favicon.ico',
+    },
+    apple: {
+      rel: 'apple-touch-icon',
+      url: '/static/favicons/apple-touch-icon.png',
+      sizes: '180x180',
+    },
+  },
+  appleWebApp: {
+    title: siteConfig.shortName,
+  },
+  manifest: '/static/favicons/site.webmanifest',
 }
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
-
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
   return (
     <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
