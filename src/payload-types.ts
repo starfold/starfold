92b8ec7355f79d36e740464e509cab1271f46781
Xiao Hanyu <xiaohanyu1988@gmail.com>
2026-04-07 20:35:56 +0800

    chore: add missing sharp deps for payload Media collection

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -90,7 +90,7 @@ export interface Config {
       | PayloadMigrationsSelect<true>
   }
   db: {
-    defaultIDType: string
+    defaultIDType: number
   }
   fallbackLocale: null
   globals: {}
@@ -128,7 +128,7 @@ export interface UserAuthOperations {
  * via the `definition` "users".
  */
 export interface User {
-  id: string
+  id: number
   updatedAt: string
   createdAt: string
   email: string
@@ -153,7 +153,7 @@ export interface User {
  * via the `definition` "media".
  */
 export interface Media {
-  id: string
+  id: number
   alt: string
   updatedAt: string
   createdAt: string
@@ -172,7 +172,7 @@ export interface Media {
  * via the `definition` "payload-kv".
  */
 export interface PayloadKv {
-  id: string
+  id: number
   key: string
   data:
     | {
@@ -189,20 +189,20 @@ export interface PayloadKv {
  * via the `definition` "payload-locked-documents".
  */
 export interface PayloadLockedDocument {
-  id: string
+  id: number
   document?:
     | ({
         relationTo: 'users'
-        value: string | User
+        value: number | User
       } | null)
     | ({
         relationTo: 'media'
-        value: string | Media
+        value: number | Media
       } | null)
   globalSlug?: string | null
   user: {
     relationTo: 'users'
-    value: string | User
+    value: number | User
   }
   updatedAt: string
   createdAt: string
@@ -212,10 +212,10 @@ export interface PayloadLockedDocument {
  * via the `definition` "payload-preferences".
  */
 export interface PayloadPreference {
-  id: string
+  id: number
   user: {
     relationTo: 'users'
-    value: string | User
+    value: number | User
   }
   key?: string | null
   value?:
@@ -235,7 +235,7 @@ export interface PayloadPreference {
  * via the `definition` "payload-migrations".
  */
 export interface PayloadMigration {
-  id: string
+  id: number
   name?: string | null
   batch?: number | null
   updatedAt: string
